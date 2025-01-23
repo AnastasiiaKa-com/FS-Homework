69a4d1fb5b7ee257f42e25ffc7924a4949ed428a
AnastasiiaKa-com <nastya.karpenko.20152012@gmail.com>
2025-01-23 08:18:13 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import '../src/App.css'
+import Homework01 from './homeworks/homework01/Homework01'
+import Homework02 from './homeworks/homework02/Homework02';
+import Homework03 from './homeworks/homework03/Homework03';
+
+function App() {
+  
+
+  return (
+    <>
+      <Homework02/>
+    </>
+  );
+}
+
+export default App;
